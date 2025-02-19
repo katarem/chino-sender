2c93d30d85347cf1a9cba811cd6469ee48007a7a
cristian.espinosa <cristian.espinosa@alithya.com>
2025-02-19 23:57:08 +0000

    feature: dockerized app

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM node:22 AS build
+
+WORKDIR /app
+
+COPY package.json .
+
+RUN npm install
+
+FROM node:22
+
+WORKDIR /app
+
+COPY --from=build /app /app
+
+COPY . .
+
+CMD ["npm", "start"]
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const path = require("path");
 const token = process.env.CLIENT_SECRET;
 const target = process.env.TARGET_ID;
 const file = path.join(__dirname, "../message.md");
+const gif = process.env.GIF_URL;
 
 const main = async () => {
 
@@ -22,13 +23,16 @@ const main = async () => {
     })
 
     client.login(token);
-    const responseDelay = 24 * 3600 * 1000; // 1 día
+   const responseDelay = 24 * 3600 * 1000; // 1 día
 
     // 1 dia son 24h 1h son 3600s por ende sería 24 * 3600 * 1000 
     setInterval(async () => {
+        
         const user = await client.users.fetch(target);
-        await user.send( message.substring(0, 1854));
-        await user.send( message.substring(1854, message.length));
+        await user.send(message.substring(0, 1854));
+        await user.send(message.substring(1854, message.length));
+        await user.send(gif);
+
     }, responseDelay);
 }
 
